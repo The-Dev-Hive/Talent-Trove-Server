9df4377e8be75958dc8946bf58775cb125dd947e
yeasin2002 <mdkawsarislam2002@gmail.com>
2024-09-28 01:34:45 +0600

    chore: added swcrc for application build

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import tseslint from 'typescript-eslint';
 export default [
   { files: ['src/*.{js,mjs,cjs,ts}'] },
   { languageOptions: { globals: globals.browser } },
+
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
